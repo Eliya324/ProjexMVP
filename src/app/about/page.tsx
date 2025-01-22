8adabaa73f725ecb389de1b1450434da928411a5
Eliya324 <Eliyachayshoval@gmail.com>
2025-01-22 13:32:33 +0200

    Setup project
    First push that setup MVP projex

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AboutPage() {
+   // By defult component are server component - they runder in a server so we can connect here to the DB
+    // for example you can expect that console.log print in client console, 
+    // BUT he print in server console
+    // console.log("hey from about page");
+  return (
+    <div>
+      AboutPage
+    </div>
+  )
+}
+
+export default AboutPage
