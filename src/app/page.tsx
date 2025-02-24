e1de4009d6e6744198a41590ff78f9b23d705ac0
= <=>
2025-02-24 15:39:13 +0200

    Resolve merge conflict in src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import ProjectCard from "@/components/ui/ProjectCard";
+import UserCard from "@/components/ui/UserCard";
 import Image from "next/image";
 import { Button } from "@/components/ui/Button";
 export default function Home() {
   return (
     <div>
-      <div className="flex justify-center items-center h-screen bg-gray-100">
-      </div>
+
+      <UserCard></UserCard>
+      <ProjectCard></ProjectCard>
+      <h1>Projex MVP</h1>
     </div>
   );
 }
